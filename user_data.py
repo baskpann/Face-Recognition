6416f3db87ae116d38b4e63dc05cfcdfa5d343b7
Baskaran Panneerselvam <baskpann@gmail.com>
2020-11-02 21:14:39 +0530

    Added code files

---
+++ user_data.py
@@ -0,0 +1,17 @@
+name = {
+    0 : {
+        "id" : "2017IT0995",
+        "name" : "Baskaran",
+        "mobile" : "9843552065",
+    },
+
+    1 : {
+        "id" : "2017IT0080",
+        "name" : "Kaaviya Baskaran",
+        "mobile" : "9940217001",
+    },
+
+    2 : {
+        # new user
+    },
+}
